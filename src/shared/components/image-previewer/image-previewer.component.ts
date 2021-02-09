6375a700f06adc9443e4f707a8bec85500a6e20a
Matias Oliva <olivamati@gmail.com>
2021-02-09 18:44:13 -0300

    added a output event from image-previewer and clear preview when a user upload a new image

--- src/shared/components/image-previewer/image-previewer.component.ts
+++ src/shared/components/image-previewer/image-previewer.component.ts
@@ -1,4 +1,6 @@
-import { Component, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+import { Component, ViewChild, ElementRef, Renderer2, Output, EventEmitter } from '@angular/core';
+
+
 
 @Component({
   selector: 'shared-image-previewer',
@@ -7,12 +9,17 @@ import { Component, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 })
 export class ImagePreviewerComponent {
 
+  @Output() files: EventEmitter<any> = new EventEmitter();
   @ViewChild('images') images: ElementRef;
 
-  constructor( private renderer: Renderer2 ) { }
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
 
   fileInputchange(event) {
 
+    if (this.images.nativeElement.hasChildNodes()) {
+      this.deleteAllChildNodes(this.images.nativeElement);
+    }
+    
     for (const fileKey in event.target.files) {
       const reader: FileReader = new FileReader();
 
@@ -27,10 +34,16 @@ export class ImagePreviewerComponent {
 
           this.renderer.setStyle(image, 'width', '150px');
           this.renderer.setStyle(image, 'height', '200px');
-        };  
+        };
       }
+      this.files.emit(event.target.files);
     }
   }
 
 
+  private deleteAllChildNodes(parentNode): void {
+    while (parentNode.firstChild) {
+      parentNode.removeChild(parentNode.firstChild);
+    }
+  }
 }
