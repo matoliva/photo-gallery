e6c01a1172b499445175591e419e78b1feeb5610
Matias Oliva <olivamati@gmail.com>
2021-02-10 23:01:26 -0300

    added logic to delete a imagePreview

--- src/shared/components/image-previewer/image-previewer.component.ts
+++ src/shared/components/image-previewer/image-previewer.component.ts
@@ -7,13 +7,14 @@ import { Component, ViewChild, ElementRef, Renderer2, Output, EventEmitter } fro
 })
 export class ImagePreviewerComponent {
 
-  @Output() files: EventEmitter<FileReader> = new EventEmitter();
+  @Output() files: EventEmitter<FileReader[]> = new EventEmitter();
   @ViewChild('images') images: ElementRef;
 
   imageError = false;
 
   acceptedFormats: string[] = ['.jpg', 'jpeg', '.png'];
   allowedFormatsFileReader: string[] = ['image/jpg', 'image/jpeg', 'image/png'];
+  fileReaderArray;
 
   constructor(private renderer: Renderer2) { }
 
@@ -21,8 +22,8 @@ export class ImagePreviewerComponent {
     this.cleanPreviews();
 
     if (this.isValidImage(event)) {
-      this.createPreviewImages(event);
-      this.files.emit(event.target.files);
+      this.fileReaderArray = this.createPreviewImages(event);
+      this.files.emit(this.fileReaderArray);
     }
   }
 
@@ -39,23 +40,49 @@ export class ImagePreviewerComponent {
     this.imageError = false;
   }
 
-  private createPreviewImages(event): void {
+  private createPreviewImages(event): FileReader[] {
+    const readers: FileReader[] = [];
+
     for (const file of event.target.files) {
-      const reader: FileReader = new FileReader();
+      const reader = new FileReader();
 
       reader.readAsDataURL(file);
 
       reader.onload = () => {
         const image: ElementRef = this.renderer.createElement('img');
-        this.renderer.listen(image, 'click', () => { alert('I was clicked'); });
+        this.renderer.listen(image, 'click', () => { this.deletePreview(file.name); });
 
         this.renderer.setProperty(image, 'src', reader.result);
+        this.renderer.setProperty(image, 'id', file.name);
         this.renderer.appendChild(this.images.nativeElement, image);
 
-        this.renderer.setStyle(image, 'width', '150px');
-        this.renderer.setStyle(image, 'height', '200px');
+        this.renderer.setStyle(image, 'width', '75px');
+        this.renderer.setStyle(image, 'height', '100px');
+        this.renderer.setStyle(image, 'opacity', '0.6');
+        this.renderer.setStyle(image, 'cursor', 'pointer');
+        this.renderer.setStyle(image, 'margin', '0.25rem');
       };
+      readers.push(file);
     }
+    return readers;
+  }
+
+  private deletePreview(fileNameToRemove: string): void {
+    let indexToRemove;
+    const parentNode = document.getElementById('images');
+    const previewImageNode = document.getElementById(fileNameToRemove);
+
+    this.fileReaderArray.forEach( (file, index) => {
+      if (file.name === fileNameToRemove) {
+        indexToRemove = index;
+        this.renderer.removeChild(parentNode, previewImageNode);
+      }
+    });
+
+    if (indexToRemove || indexToRemove === 0) {
+      this.fileReaderArray.splice(indexToRemove, 1);
+    }
+    this.files.emit(this.fileReaderArray);
   }
 
   private isValidImage(event): boolean {
